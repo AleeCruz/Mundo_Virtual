716af1f78cbc7f8fc37f871e3705e2426ab240d1
Alexander <acruz@fi.uba.ar>
2026-01-05 12:46:08 -0300

    Change: Renombrado de proyecto a Mundo_Virtual

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Trabajo_Practico_1/', // ¡Muy importante que coincida con el nombre de tu repo!
+  base: '/Mundo_Virtual/', // ¡Muy importante que coincida con el nombre de tu repo!
   plugins: [],
 })
